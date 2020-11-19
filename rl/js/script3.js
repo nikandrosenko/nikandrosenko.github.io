c5a3236769b20f1a9de89319e55317d363911390
Nikita Androsenko <varzaid@gmail.com>
2020-11-19 07:40:45 +0300

    delete again

--- rl/js/script3.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.navbar__burger').click(function(event) { 
-    // e.preventDefault();
-    $('.navbar__burger,.navbar__menu').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
-});
